83df20eb7d70c2ba2ea118e4119f3996cf7b63d2
victorrf <victorrf2002@gmail.com>
2024-12-18 19:41:20 -0500

    Test fetching OpenWeatherMap Api, successfully getting current temperature

--- script/main.js
+++ script/main.js
@@ -6,4 +6,39 @@ currentLocation.addEventListener('click', () => {
     locationDialog.showModal();
 })
 
+// User Preferences
+var myLocation = "montreal"; // fix currentLocation above later
+var myLat = 45.51; // look up geocoding api by openweathermap later for location functionality without needing latitude and longitude
+var myLon = -73.56;
+var units = "metric";
+
+// Data
+var currentTemp = 0;
+
+
+
 // Fetch API from OpenWeatherMap
+
+const key = "a2b8af37466283fe470ff2d6ee8699fc";
+
+fetchData();
+
+async function fetchData() {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLon}&appid=${key}&units=${units}`);
+
+        if (!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+
+        currentTemp = Math.round(data.list[0].main.temp);
+
+        console.log("Current Temperature is: " + currentTemp + "°C");
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
