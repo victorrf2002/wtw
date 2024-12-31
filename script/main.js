16ec9d37852c21e292e9a21b1445a7fab2c04287
victorrf <victorrf2002@gmail.com>
2024-12-30 20:39:28 -0500

    Dynamically update weather icon given the weather condition

--- script/main.js
+++ script/main.js
@@ -14,6 +14,7 @@ var units = "metric";
 
 // Data
 var currentTemp = 0;
+var condition = "";
 
 
 
@@ -34,12 +35,72 @@ async function fetchData() {
         const data = await response.json();
 
         currentTemp = Math.round(data.list[0].main.temp);
+        condition = data.list[0].weather[0].main;
 
+
+
+        console.log("Current Weather condition is: " + condition);
         console.log("Current Temperature is: " + currentTemp + "°C");
+
         document.getElementById("current-weather-degrees").innerHTML = currentTemp + "°C";
+        getCondition(condition);
+
+
     }
     catch (error) {
         console.error(error);
+    }
+}
+
+function getCondition(condition) {
+    switch (condition) {
+        case "Rain":
+            document.getElementById("current-weather-icon").src = "assets/svg/umbrella-svgrepo-com.svg";
+            break;
+        case "Thunderstorm":
+            document.getElementById("current-weather-icon").src = "assets/svg/thunder-svgrepo-com.svg";
+            break;
+        case "Drizzle":
+            document.getElementById("current-weather-icon").src = "assets/svg/rain-water-svgrepo-com.svg";
+            break;
+        case "Snow":
+            document.getElementById("current-weather-icon").src = "assets/svg/snowflake-svgrepo-com.svg";
+            break;
+        case "Clear":
+            document.getElementById("current-weather-icon").src = "assets/svg/sun-svgrepo-com.svg";
+            break;
+        case "Clouds":
+            document.getElementById("current-weather-icon").src = "assets/svg/cloud-svgrepo-com.svg";
+            break;
+        case "Mist":
+            document.getElementById("current-weather-icon").src = "assets/svg/cloudy-cloud-svgrepo-com.svg";
+            break;
+        case "Smoke":
+            document.getElementById("current-weather-icon").src = "assets/svg/storm-dust-svgrepo-com.svg";
+            break;
+        case "Haze":
+            document.getElementById("current-weather-icon").src = "assets/svg/cloudy-sun-svgrepo-com.svg";
+            break;
+        case "Dust":
+            document.getElementById("current-weather-icon").src = "assets/svg/storm-dust-svgrepo-com.svg";
+            break;
+        case "Fog":
+            document.getElementById("current-weather-icon").src = "assets/svg/cloudy-cloud-svgrepo-com.svg";
+            break;
+        case "Sand":
+            document.getElementById("current-weather-icon").src = "assets/svg/storm-dust-svgrepo-com.svg";
+            break;
+        case "Ash":
+            document.getElementById("current-weather-icon").src = "assets/svg/volcano-svgrepo-com.svg";
+            break;
+        case "Squall":
+            document.getElementById("current-weather-icon").src = "assets/svg/tornado-svgrepo-com.svg";
+            break;
+        case "Tornado":
+            document.getElementById("current-weather-icon").src = "assets/svg/tornado-svgrepo-com.svg";
+            break;
+        default:
+            document.getElementById("current-weather-icon").src = "assets/svg/sun-svgrepo-com.svg";
     }
 }
 
